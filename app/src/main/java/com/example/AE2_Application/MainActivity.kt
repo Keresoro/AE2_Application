2fd19ac41b8dfd5d7dd3b1dead9611bcb70b8bfd
Keresoro <38075826+Keresoro@users.noreply.github.com>
2019-05-06 10:21:55 +0100

    Initial commit

---
+++ app/src/main/java/com/example/AE2_Application/MainActivity.kt
@@ -0,0 +1,51 @@
+package com.example.AE2_Application
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var helper: MyHelper
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar1)
+
+        helper = MyHelper(ctx = this)
+        Add.setOnClickListener {
+            val title = BTitle.text.toString()
+            val artist = BArtist.text.toString()
+            val year = BYear.text.toString().toLong()
+            val id = helper.insert(title, artist, year)
+            Toast.makeText( this,"You successfully added this song", Toast.LENGTH_LONG).show()
+        }
+        Search.setOnClickListener {
+            val id = BId.text.toString().toLong()
+            val database = helper.search(id)
+            BTitle.setText(database?.title)
+            BArtist.setText(database?.artist)
+            BYear.setText(database?.year.toString())
+            Toast.makeText( this,"You successfully searched for this song", Toast.LENGTH_LONG).show()
+        }
+        Uptade.setOnClickListener {
+            val id = BId.text.toString().toLong()
+            val title = BTitle.text.toString()
+            val artist = BArtist.text.toString()
+            val year = BYear.text.toString().toLong()
+            val uptade = helper.uptade(id, title, artist, year)
+        }
+        Delete.setOnClickListener {
+            val id = BId.text.toString().toLong()
+            val delete = helper.delete(id)
+        }
+    }
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+}
---
+++ app/src/main/java/com/example/AE2_Application/MyHelper.kt
@@ -0,0 +1,67 @@
+package com.example.AE2_Application
+
+import android.database.sqlite.SQLiteOpenHelper
+import android.database.sqlite.SQLiteDatabase
+import android.content.Context
+
+data class Song (val id: Long, val title: String, val artist: String, val year: Long)
+class MyHelper(ctx:Context) : SQLiteOpenHelper(ctx,"MyDatabase", null, 1) {
+
+
+    override fun onCreate(db: SQLiteDatabase) {
+        db.execSQL ("CREATE TABLE IF NOT EXISTS People (Id INTEGER PRIMARY KEY, Title VARCHAR(255), Artist VARCHAR(255), Year LONG)")
+    }
+
+    override fun onUpgrade(db:SQLiteDatabase, oldVersion:Int, newVersion:Int) {
+        db.execSQL ("DROP TABLE IF EXISTS People")
+        onCreate(db)
+    }
+
+    fun insert(title: String, artist: String, year: Long) : Long{
+        val db = writableDatabase
+        val stmt = db.compileStatement ("INSERT INTO People (Title, Artist, Year) VALUES (?, ?, ?)");
+        stmt.bindString (1, title)
+        stmt.bindString (2, artist)
+        stmt.bindLong (3, year)
+        val id = stmt.executeInsert()
+        return id
+    }
+    fun search(id: Long) : Song? {
+        val db = readableDatabase
+        val cursor = db.rawQuery("SELECT * FROM People WHERE Id=?", arrayOf(id.toString()) )
+            if (cursor.moveToFirst()) {
+                val p = Song ( cursor.getLong(cursor.getColumnIndex("Id")),
+                    cursor.getString(cursor.getColumnIndex("Title")),
+                    cursor.getString(cursor.getColumnIndex("Artist")),
+                    cursor.getLong(cursor.getColumnIndex("Year"))
+                )
+                cursor.close()
+                return p
+            }
+                cursor.close()
+                return null
+    }
+    fun uptade(id: Long, title: String, artist: String, year: Long) : Int {
+        val db = writableDatabase
+        val stmt = db.compileStatement ("UPDATE People SET Title=?, Artist=?, Year=? WHERE Id=?");
+        stmt.bindString (1, title)
+        stmt.bindString (2, artist)
+        stmt.bindLong (3, year)
+        stmt.bindLong(4, id)
+        val nAffectedRows = stmt.executeUpdateDelete();
+        return nAffectedRows
+    }
+    fun delete(id: Long) : Int {
+        val db = writableDatabase
+        val stmt = db.compileStatement  ("DELETE FROM People WHERE Id=?");
+        stmt.bindLong(1, id)
+        val nAffectedRows = stmt.executeUpdateDelete();
+        return nAffectedRows
+    }
+
+}
+
+
+
+
+
